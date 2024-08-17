6e697e95237f02c07206eca403cf20cc74812775
fanyang <2940276020@qq.com>
2024-08-17 19:38:24 +0800

    add python script, create_post.py

---
+++ create_post.py
@@ -0,0 +1,36 @@
+import os
+# import pytz
+from tzlocal import get_localzone
+import argparse
+from datetime import datetime
+
+def create_post(title):
+    # get time
+    today=datetime.now(get_localzone()).strftime('%Y-%m-%d')
+    date=datetime.now(get_localzone()).strftime('%Y-%m-%d %H:%M:%S %z')
+
+    # get standard file name
+    # probably Chinese could be handled
+    fileName=f"{today}-{title.replace(' ','-').lower()}.md"
+
+    # get config data of a usual post
+    front_matter=f"""---
+layout: post
+title: "{title}"
+date: {date} 
+categories: []
+tags: []
+---
+"""
+
+    # make file
+    with open(f"_posts/{fileName}",'w',encoding='utf-8') as file:
+        file.write(front_matter)
+
+    print(f"Create post: {fileName}")
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument('title',type=str,help="文章标题")
+    args=parser.parse_args()
+    create_post(args.title)
