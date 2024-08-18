7ea949d9802a8058d430e3285ef4b7b396b9a8be
fanyang <2940276020@qq.com>
2024-08-18 09:26:47 +0800

    add script function to create draft

--- ctp.bat
+++ ctp.bat
@@ -1,4 +1,11 @@
 @echo off
 :: ctp, i.e.create post
 :: call create_post.py
-python .\pyScripts\create_post.py %1
+:: if %1=--draft create draft
+if "%2"=="" (
+    python .\pyScripts\create_post.py %1
+) else if "%1"=="--draft" (
+    python .\pyScripts\create_post.py %2 "True"
+) else (
+    echo \033[31mwrong command arguments\033[0m
+)
--- pyScripts/create_post.py
+++ pyScripts/create_post.py
@@ -4,7 +4,7 @@ from tzlocal import get_localzone
 import argparse
 from datetime import datetime
 
-def create_post(title):
+def create_post(title,draft=False):
     # get time
     today=datetime.now(get_localzone()).strftime('%Y-%m-%d')
     date=datetime.now(get_localzone()).strftime('%Y-%m-%d %H:%M:%S %z')
@@ -24,13 +24,19 @@ tags: []
 """
 
     # make file
-    with open(f"_posts/{fileName}",'w',encoding='utf-8') as file:
-        file.write(front_matter)
+    if draft==False:
+        with open(f"_posts/{fileName}",'w',encoding='utf-8') as file:
+            file.write(front_matter)
+        print(f"\033[33mCreate post:\033[0m \033[34m{fileName}\033[0m")
+    else :
+        with open(f"_drafts/{fileName}",'w',encoding='utf-8') as file:
+            file.write(front_matter)
+        print(f"\033[33mCreate draft:\033[0m \033[34m{fileName}\033[0m")
 
-    print(f"\033[33mCreate post:\033[0m \033[34m{fileName}\033[0m")
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument('title',type=str,help="文章标题")
+    parser.add_argument('draft',nargs='?',type=bool, default=False, help="是否是草稿")
     args=parser.parse_args()
-    create_post(args.title)
+    create_post(args.title,args.draft)
